edb54f7ee440983e37da2a8803a166544b5ecd24
woo37830 <jwooten37830@me.com>
2020-01-23 14:05:59 -0500

    added date to fields saved

--- save_book.php
+++ save_book.php
@@ -8,11 +8,12 @@ $header = htmlspecialchars($_REQUEST['header']);
 $page = htmlspecialchars($_REQUEST['page']);
 $data = htmlspecialchars($_REQUEST['data']);
 $image_url = htmlspecialchars($_REQUEST['image_url']);
+$date = htmlspecialchars($_REQUEST['date']);
 
 include 'conn.php';
 
-$stmt = $conn->prepare("insert into books(paper,measurement,header,page,data,user,image_url) values(?,?,?,?,?,?,?)");
-$stmt->bind_param("sisisss",$paper,$measurement,$header,$page,$data,$user,$image_url);
+$stmt = $conn->prepare("insert into books(paper,measurement,header,page,data,user,image_url,date) values(?,?,?,?,?,?,?,?)");
+$stmt->bind_param("sisisss",$paper,$measurement,$header,$page,$data,$user,$image_url,$date);
 $result = $stmt->execute();
 
 if ($result){
@@ -22,7 +23,8 @@ if ($result){
 		'header' => $header,
 		'page' => $page,
 		'data' => $data,
-		'image_url' => $image_url
+		'image_url' => $image_url,
+		'date' => $date
 	));
 } else {
 	echo json_encode(array('errorMsg'=>'Some errors occured: ' ));
