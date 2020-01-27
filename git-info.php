eef99f4e90af203c3bef3522188fc13c2dc1c365
woo37830 <jwooten37830@me.com>
2020-01-27 16:42:36 -0500

    Added last update

--- git-info.php
+++ git-info.php
@@ -1,5 +1,6 @@
 <?php
+   $last = exec('git log -1 --date=format:"%Y/%m/%d" --format="%ad"');
    $rev = exec('git rev-parse --short HEAD');
    $branch = exec('git rev-parse --abbrev-ref HEAD');
-   echo "<center>Commit: $rev &nbsp;&nbsp;&nbsp;Branch: $branch </center>";
+   echo "<center>Last Update: $last &nbsp;&nbsp;Commit: $rev &nbsp;&nbsp;&nbsp;Branch: $branch </center>";
 ?>
